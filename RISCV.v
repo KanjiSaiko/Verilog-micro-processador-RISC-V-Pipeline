cc5ae1a46d3cb548b24b01d44544b0ec31e31d61
KanjiSaiko <hbortolomiol2@gmail.com>
2025-05-20 12:01:52 -0300

    codigo modularizado e consertado alguns erros

--- RISCV.v
+++ RISCV.v
@@ -3,145 +3,149 @@ module RISCV (clock, reset);
     input clock;
     input reset;
 
+
+//memórias e registradores
 reg [15:0] mem_instruc [0:255]; //memoria de instruções com 256 posições de tamanho 16 bits
 reg [7:0] mem_dados [0:255]; //memoria de dados com 256 posições de tamanho 8 bits
-reg [7:0] banco_regs [0:31]; //banco de 32 registradores de 8 bits
+reg [7:0] banco_regs [0:15]; //banco de 32 registradores de 8 bits
 
-task automatic zera_meminstruc (output reg[15:0] mem_instruc [0:255]);
+task zera_meminstruc;
     integer i;
     begin
-        for(i = 0; i < 255; i = i+1) //for para limpar a memoria de instruções com valor 0
-            mem_instruc[i] = 16'b0;
-            $display("mem_dados[%0d] = 0x%0h", i, mem_instruc[i]);
+      for (i = 0; i < 256; i = i + 1) begin
+        mem_instruc[i] = 16'b0;
+        $display("mem_instruc[%0d]=0x%0h", i, mem_instruc[i]);
+      end
     end
 endtask
 
-task automatic zera_memdados (output reg[7:0] mem_dados [0:255]);
+task zera_memdados;
     integer i;
     begin
-        for(i = 0; i < 255; i = i+1) //for para limpar a memoria de dados com valor 0
-            mem_dados[i] = 8'b0;
-            $display("mem_dados[%0d] = 0x%0h", i, mem_dados[i]);
+      for (i = 0; i < 256; i = i + 1) begin
+        mem_dados[i] = 8'b0;
+        $display("mem_dados[%0d]=0x%0h", i, mem_dados[i]);
+      end
     end
 endtask
 
-task automatic zera_regs (output reg[7:0] banco_regs [0:15]);
+task zera_regs;
     integer i;
     begin
-        for(i = 0; i < 15; i = i+1) //for para limpar o banco de registradores com valor 0
-            banco_regs[i] = 8'b0;
-            $display("mem_dados[%0d] = 0x%0h", i, banco_regs[i]);
+      for (i = 0; i < 16; i = i + 1) begin
+        banco_regs[i] = 8'b0;
+        $display("banco_regs[%0d]=0x%0h", i, banco_regs[i]);
+      end
     end
 endtask
 
-//Considerar atribuir esta task direto em zerar instruções
-task automatic atribui_instrucao (output reg[15:0] mem_instruc [0:255]);
-    mem_instruc[0] = 16'b0110000000001010; // BNE R0 != R1 FALSO
-    mem_instruc[1] = 16'b0000000000000001; // LDA endereço 1 para R0 (Valor 1)
-    mem_instruc[2] = 16'b0000000100000010; // LDA endereço 2 para R1 (Valor 3)
-    mem_instruc[3] = 16'b1010100000000111; // LWI Valor 7 no registrador 8
-    mem_instruc[4] = 16'b1111111111111111; // Bolha artificial
+task atribui_instrucao; //Considerar atribuir esta task direto em zerar instruções
+    begin
+        mem_instruc[0] = 16'b0000000100100110; // LW s2, N
+        mem_instruc[1] = 16'b0001000000110010; // addi s3, x0, 1 
+        mem_instruc[2] = 16'b0010001100000011; // bge s3, s2, finish
+        mem_instruc[3] = 16'b1010100000000111; // addi s4, x0, 0
+    end
 endtask
 
-//Considerar atribuir esta task direto em zerar dados
-task automatic atribui_dados (output reg[7:0] mem_dados [0:255]);
-    mem_dados[1] = 8'b00000001; // Valor 1
-    mem_dados[2] = 8'b00000011; // Valor 3
+task atribui_dados; //Considerar atribuir esta task direto em zerar dados
+    begin
+        mem_dados[1] = 8'b00000111; // Valor 7
+        mem_dados[2] = 8'b00000011; // Valor 3
+    end
 endtask
-
-reg [7:0] PC; //Contador
-reg greater_or_equal, less; //Sinais para os Branch's (BGE e BLT).
-reg desvio;
-reg [7:0] ulaEX_MEM;
-reg [3:0] R1ID_EX, R2ID_EX, R2EX_MEM;
-reg [3:0] R2wID_EX, R2wEX_MEM, R2wMEM_WB; //Registrador a ser escrito
-reg [7:0] PCIF_ID, PCID_EX, PCEX_MEM, PCMEM_WB //Contador de programa (Program Counter) que armazena o endereço atual de execuçao.
-reg [15:0] InIF_ID, InID_EX, InEX_MEM, InMEM_WB //Instrucao Atual de cada estagio
-
-//Para instrução AUIPC
-assign imm20
-assign imm_sext
-assign imm_shift
 
 initial begin //Executa de uma vez só
-    zera_memdados(mem_dados);
-    zera_meminstruc(mem_instruc);
-    zera_regs(banco_regs);
-    atribui_instrucao(mem_instruc);
-    atribui_dados(mem_dados);
+    zera_memdados;
+    zera_meminstruc;
+    zera_regs;
+    atribui_instrucao;
+    atribui_dados;
 end
 
-always @(*) begin //Lógica Combinacional
-    //Verifica se R1 é maior ou igual a R2
-    if (R1ID_EX >= R2wEX_MEM)
-        greater_or_equal <= 1;
-    else
-        greater_or_equal <= 0;
+reg greater_or_equal, less; //Sinais para os Branch's (BGE e BLT).
+reg desvio;
+reg [7:0] PC; //Contador
 
-    //Verifica se R1 é menor que R2
-    if (R1ID_EX < R2wEX_MEM)
-        less <= 1;
-    else
-        less <= 0;
+// Pipeline registers
+// IF/ID
+reg [15:0] InIF_ID;
+reg [7:0] PCIF_ID;
 
-    //Indica se um salto deve ocorrer a partir do opcode, funct3 e da condição
-    if ((InEX_MEM[3:0] == 4'b0011) and (greater_or_equal == 1)) or ((InEX_MEM[3:0] == 7'b0100) and (less == 1))
-        desvio <= 1;
-    else
-        desvio <= 0;
-end
+// ID/EX
+reg [15:0] InID_EX;
+reg [3:0] R1ID_EX, R2ID_EX, R2wID_EX;
+reg [7:0] PCID_EX;
+        //Para instrução AUIPC
+    reg [7:0] ID_EX_imm8_u;
+    wire signed [15:0] imm_sext  = {{8{ID_EX_imm8_u[7]}}, ID_EX_imm8_u};
+    wire signed [15:0] imm_shift = imm_sext <<< 4; 
+    wire greater_or_equal = (ID_EX_rs1 >= ID_EX_rs2);
+    wire less            = (ID_EX_rs1 <  ID_EX_rs2);
+    wire is_bge = (ID_EX_instr[3:0] == 4'b0011) && greater_or_equal;
+    wire is_blt = (ID_EX_instr[3:0] == 4'b0100) && less;
+    wire salto = is_bge || is_blt;
 
 
-always @(posedge clock, posedge reset) //Lógica Sequencial
-begin
-    if(reset)
-        PC <= 0;
-        PCIF_ID <= 0;
-        PCID_EX <= 0;
-        PCEX_MEM <= 0;
-        PCMEM_WB <= 0;
+// EX/MEM
+reg [7:0] ulaEX_MEM;
+reg [3:0] R2EX_MEM;
+reg [3:0] R2wEX_MEM, R2wMEM_WB; //Registrador a ser escrito
+reg [7:0] PCEX_MEM; //Contador de programa (Program Counter) que armazena o endereço atual de execuçao.
+reg [15:0] InEX_MEM; //Instrucao Atual de cada estagio
 
-        InIF_ID <= 16'b0;
-        InID_EX <= 16'b0;
-        InEX_MEM <= 16'b0;
-        InMEM_WB <= 16'b0;
+// MEM/WB
+reg [7:0] PCMEM_WB;
+reg [15:0] InMEM_WB;
 
-        ulaEX_MEM <= 8'b0;
 
-        R0ID_EX <= 8'b0;
-        R1ID_EX <= 8'b0;
-        R2wID_EX <= 8'b0;
-        R2wEX_MEM <= 8'b0;
-        R2wMEM_WB <= 8'b0;
-
-        //bloco
-    else
-        //IF_ID
-        InIF_ID <= mem_instruc[PC];
-        InID_EX <= InIF_ID;
-        InEX_MEM <= InID_EX;
-        InMEM_WB <= InEX_MEM;
-
-        PCIF_ID <= PC;
-        PCID_EX <= PCIF_ID;
-        PCEX_MEM <= PCID_EX;
-        PCMEM_WB <= PCEX_MEM;
-
-        if (desvio == 1) begin
+//Atualização do PC
+always @(posedge clock or posedge reset) begin
+    if (reset)
+      PC <= 0;
+    else if (desvio == 1) begin
             if((InEX_MEM[3:0] == 4'b0011) or (InEX_MEM[3:0] == 4'b0100)) //BGE/BLT
                 PC <= PC + InEX_MEM[15:12];
-        end
-        
-        else if(InEX_MEM[3:0] == 4'b0101) //JAL
+    end
+
+    else if(InEX_MEM[3:0] == 4'b0101) begin//JAL
             banco_regs[InEX_MEM[7:4]] <= PC + 1;
             PC <= PC + InEX_MEM[15:8];
-
+    end
+    
         else
             PC <= PC + 1; //pensar se pode haver problemas deixando +1 ao invés de +4
+  end
 
-        
-        //ID_EX
-        if(InID_EX[3:0] == 7'b0101) begin //JAL
+//IF/ID
+  always @(posedge clock or posedge reset) begin
+    if (reset) begin
+      PCIF_ID <= 0;
+      InIF_ID <= 16'b0;
+    end 
+    else begin
+      InIF_ID <= mem_instruc[PC];
+      PCIF_ID <= PC + 1;
+    end
+  end
+
+//ID/EX
+  always @(posedge clock or posedge reset) begin
+    if (reset) begin
+      PCID_EX <= 0;
+      InID_EX <= 16'b0;
+      R0ID_EX <= 8'b0;
+      R1ID_EX <= 8'b0;
+      R2wID_EX <= 8'b0;
+    end 
+    else begin
+      // extrai campos
+      InID_EX <= InIF_ID;
+      PCID_EX <= PCIF_ID;
+      ID_EX_rs2       <= IF_ID_instr[24:20];
+      ID_EX_rd        <= IF_ID_instr[11:7];
+      // imediato e shift para AUIPC, por exemplo
+      if(InID_EX[3:0] == 4'b0101) begin //JAL
             R2wID_EX <= 8'b0;
             R1ID_EX <= 8'b0;
             R2ID_EX <= 8'b0;
@@ -156,10 +160,6 @@ begin
         end
 
         else if(InID_EX[3:0] == 4'b0000) begin //AUIPC (TIPO U)
-        //modificar isso para instruções compactas
-            imm20 <= InID_EX[15:8];
-            imm_sext  = {{12{imm20[19]}}, imm20}; 
-            imm_shift = imm_sext << 12;
         end
 
         else begin //I e J (que nao utiliza)
@@ -167,9 +167,22 @@ begin
             R1ID_EX <= banco_regs[InID_EX[11:8]];
             R2wID_EX <= banco_regs[InID_EX[15:12]]; 
         end
+    end
+  end
 
-        //EX_MEM
-        if(desvio == 1) begin //bolhas
+//EX/MEM
+ always @(posedge clock or posedge reset) begin
+    if (reset) begin
+      PCEX_MEM <= 0;
+      InEX_MEM <= 16'b0;
+      ulaEX_MEM <= 8'b0;
+      R2wEX_MEM <= 8'b0;
+      
+    end 
+    else begin
+      InEX_MEM <= InID_EX;
+      PCEX_MEM <= PCID_EX;
+      if(desvio == 1) begin //bolhas
             ulaEX_MEM <= 8'b0;
             R2wID_EX <= 8'b0;
             R1ID_EX <= 8'b0;
@@ -180,7 +193,7 @@ begin
             InIF_ID <= 16'b1;
         end
 
-        else begin
+      else begin
             R2wEX_MEM <= R2wID_EX;
             R2EX_MEM <= R2ID_EX;
                 case (InEX_MEM[3:0]) //analisa opcode para realizar operações
@@ -204,10 +217,25 @@ begin
 
                     4'b1000: //SW
                         ulaEX_MEM <= R1ID_EX + InEX_MEM[7:4];
+
+                    default:
+                        ulaEX_MEM <= 8'b0;
+
                 endcase         
         end
+    end
+  end
 
-        //MEM_WB
+//MEM/WB
+ always @(posedge clock or posedge reset) begin
+    if (reset) begin
+        PCMEM_WB <= 0;
+        InMEM_WB <= 16'b0;
+        R2wMEM_WB <= 8'b0;
+    end 
+    else begin
+        InMEM_WB <= InEX_MEM;
+        PCMEM_WB <= PCEX_MEM;
         case (InMEM_WB[3:0]) //analisa opcode para realizar operações
                     4'b1000: //STORE WORD
                         mem_dados[ulaEX_MEM] <= R2wEX_MEM;
@@ -227,7 +255,9 @@ begin
                         banco_regs[InMEM_WB[[11:7]]] <= ulaEX_MEM;
                         
                 endcase 
+    end
+  end
 
-end
+
 endmodule
 
